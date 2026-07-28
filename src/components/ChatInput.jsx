4297a0c64f1bbede789ce05021bb915c8fa32f09
Dariusz Skrzypczak <Tedinho08@gmail.com>
2026-07-28 14:08:22 +0200

    design and functionality improvements

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -1,12 +1,29 @@
 import { useState } from "react";
 
 import './ChatInput.css';
+import '../data/chatbot';
 
-function ChatInput({onSubmit}) {
+function ChatInput({onSendMessage, setLoading}) {
     const [inputValue, setInputValue] = useState("");
+    
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        onSendMessage(inputValue, "user");
+        setInputValue("");
+
+        try {
+        setLoading(true);
+        const res = await Chatbot.getResponseAsync(inputValue);
+        onSendMessage(res, "ai");
+    }   finally {
+        setLoading(false);
+    }
+
+    }
 
     return ( 
-        <form className="form" onSubmit={(e) => onSubmit(e, inputValue)}>
+        <form className="form" onSubmit={handleSubmit}>
             <input className="form__input" 
             type="text" value={inputValue} 
             onChange={(e) => setInputValue(e.target.value)} 
--- src/components/ChatMessages.jsx
+++ src/components/ChatMessages.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useRef } from "react";
+
 import ChatMessage from "./ChatMessage";
+import Spinner from '../assets/loading-spinner.gif'
 
-function ChatMessages({messages}) {
-    const content = messages.map(message => <ChatMessage key={message.id} msg={message.msg} sender={message.sender} />);
+import './ChatMessages.css';
 
-    return content
+function ChatMessages({messages, loading}) {
+
+    const content = messages.length > 0 ? messages.map(message => <ChatMessage key={message.id} msg={message.msg} sender={message.sender} />) : <p className="welcome-message">"Welcome to the chatbot project! Send a message using the textbox below"</p>;
+
+    const messagesContainerRef = useRef(null);
+
+    useEffect(() => {
+       const messagesContainer = messagesContainerRef.current;
+       if(messagesContainer) {
+       messagesContainer.scrollTop = messagesContainer.scrollHeight;
+       }    
+    }, [messages])
+
+    return (
+        <div className="messages-container" ref={messagesContainerRef}>
+            {content}
+            {loading ? <div className='message'><img className="message__spinner" src={Spinner}></img></div> : ""}
+        </div>
+    )
 }
 
 export default ChatMessages;
